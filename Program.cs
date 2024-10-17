2de190ca1d590a0df83553df85acfa5d8c66cef3
Titvt <tqydyqt@126.com>
2024-10-17 15:37:27 +0800

    initialize

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace ElegantAI;
+
+public static class Program
+{
+    public static void Main(string[] args) => AppBuilder.Configure<App>()
+        .UsePlatformDetect()
+        .WithInterFont()
+        .Start(AppMain, args);
+
+    private static void AppMain(Application app, string[] args)
+    {
+        var cancellationTokenSource = new CancellationTokenSource();
+        new MainWindow().Show();
+        app.Run(cancellationTokenSource.Token);
+    }
+}
