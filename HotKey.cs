3ffacd5d26e212f9ae81f0e189b488717d5fad54
Titvt <tqydyqt@126.com>
2024-10-22 16:55:16 +0800

    add hot key and native

---
+++ HotKey.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Avalonia.Input;
+using Avalonia.Win32.Input;
+
+namespace ElegantAI;
+
+public static class HotKey
+{
+    private static readonly Native.WndProcDelegate LpfnWndProc = WndProc;
+    private static readonly IntPtr HWnd;
+    private static readonly Dictionary<int, Action> HotKeys = new();
+
+    static HotKey()
+    {
+        var wndClass = new Native.WndClassExW
+        {
+            cbSize = (uint)Marshal.SizeOf<Native.WndClassExW>(),
+            lpfnWndProc = LpfnWndProc,
+            hInstance = Native.GetModuleHandleW(null),
+            lpszClassName = Guid.NewGuid().ToString()
+        };
+        HWnd = Native.CreateWindowExW(0, Native.RegisterClassExW(wndClass), null, 0, 0, 0, 0, 0, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+    }
+
+    public static void Register(Key key, KeyModifiers keyModifiers, Action action)
+    {
+        var virtualKey = KeyInterop.VirtualKeyFromKey(key);
+        var id = virtualKey + ((int)keyModifiers << 16);
+        Native.RegisterHotKey(HWnd, id, (uint)keyModifiers, (uint)virtualKey);
+        HotKeys.Add(id, action);
+    }
+
+    public static void Unregister(Key key, KeyModifiers keyModifiers)
+    {
+        var virtualKey = KeyInterop.VirtualKeyFromKey(key);
+        var id = virtualKey + ((int)keyModifiers << 16);
+        Native.UnregisterHotKey(HWnd, id);
+        HotKeys.Remove(id);
+    }
+
+    private static int WndProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam)
+    {
+        if (uMsg == 0x0312 && HotKeys.TryGetValue(wParam.ToInt32(), out var action))
+        {
+            action();
+        }
+
+        return Native.DefWindowProcW(hWnd, uMsg, wParam, lParam);
+    }
+}
---
+++ Native.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace ElegantAI;
+
+public static class Native
+{
+    public delegate int WndProcDelegate(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WndClassExW
+    {
+        public uint cbSize;
+        public uint style;
+        public WndProcDelegate lpfnWndProc;
+        public int cbClsExtra;
+        public int cbWndExtra;
+        public IntPtr hInstance;
+        public IntPtr hIcon;
+        public IntPtr hCursor;
+        public IntPtr hbrBackground;
+        public string lpszMenuName;
+        public string lpszClassName;
+        public IntPtr hIconSm;
+    }
+
+    [DllImport("user32.dll")]
+    public static extern int DefWindowProcW(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetModuleHandleW(string? lpModuleName);
+
+    [DllImport("user32.dll")]
+    public static extern ushort RegisterClassExW(WndClassExW wndClass);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr CreateWindowExW(uint dwExStyle, ushort lpClassName, string? lpWindowName, uint dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
+
+    [DllImport("user32.dll")]
+    public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+    [DllImport("user32.dll")]
+    public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+}
